f867c5a83e5805d54011f59b9b033dff84b15625
laliMaryLeticinha231dsnFabisDil <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2023-10-06 16:51:55 -0300

    gg

--- main.js
+++ main.js
@@ -12,14 +12,63 @@ class GerenciadorContatos {
     constructor() {
         this.contatos = []
     }
-    adicionarContatos(contato) {   //adiciona um contato na lista de contatos
+    adicionarContato(contato) {   //adiciona um contato na lista de contatos
         this.contatos.push(contato);
     }
 
     exibirContatos() {
-        const listaContatos = document.getElementById('contato-lista')
+        const listaContatos = document.getElementById('contato-lista');
 
         //limpa a lista de contatos
-        listaContatos.innerHTML = ''
+        listaContatos.innerHTML = '';
+
+        //para cada novo contato na lista de contatos, criaum novo elemento 'li e adiciona na lista de ctts
+
+        for (const contato of this.contatos) {
+            const li = document.createElement('li');
+            li.innerHTML = `${contato.nome} - ${contato.email} - ${contato.telefone}`;
+
+            listaContatos.appendChild(li)
+        }
+
     }
 }
+
+
+const gerenciadorContatos = new GerenciadorContatos();
+const contatoForm = document.getElementById('contato-Form');
+const nomeForm = document.getElementById('nome');
+const emailForm = document.getElementById('email');
+const telefoneForm = document.getElementById('tel');
+const mostrarContatos = document.getElementById('mostrar-contatos');
+const ocultarContatos = document.getElementById('ocultar-contatos');
+const listaContatos = document.getElementById('contato-lista');
+
+contatoForm.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    const nome = nomeForm.value;
+    const email = emailForm.value;
+    const telefone = telefoneForm.value;
+
+    //criando objeto na classe contato
+
+    const contato = new Contatos(nome, email, telefone);
+
+    gerenciadorContatos.adicionarContato(contato);
+
+    nomeForm.value = ''
+    emailForm.value = ''
+    telefoneForm.value = ''
+
+});
+
+
+mostrarContatos.addEventListener('click', function(){
+    gerenciadorContatos.exibirContatos();
+    listaContatos.style.display = 'block';
+
+})
+
+ocultarContatos.addEventListener('click', function(){
+    listaContatos.style.display = 'none'
+})
